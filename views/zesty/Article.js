6c4fd0b330bf976b810fd6a84fd74ceeda4ec91b
b-estevez <bryanestevez1114@gmail.com>
2023-04-29 00:22:23 +0800

    Initial commit from Create Next App

---
+++ views/zesty/Article.js
@@ -0,0 +1,43 @@
+/**
+  * Zesty.io Content Model Component
+  * When the ZestyLoader [..slug].js file is used, this component will autoload if it associated with the URL
+  *
+  * Label: Articles
+  * Name: articles
+  * Model ZUID: 6-ba898da088-f04p2s
+  * File Created On: Sat Apr 29 2023 00:22:22 GMT+0800 (Singapore Standard Time)
+  *
+  * Model Fields:
+  *
+   * title (text)
+ * content (wysiwyg_basic)
+ * targeted_personas (one_to_many)
+ * image (images)
+  *
+  * In the render function, text fields can be accessed like {content.field_name}, relationships are arrays,
+  * images are objects {content.image_name.data[0].url}
+  *
+  * This file is expected to be customized; because of that, it is not overwritten by the integration script.
+  * Model and field changes in Zesty.io will not be reflected in this comment.
+  *
+  * View and Edit this model's current schema on Zesty.io at https://8-dcb09bb3a4-ld44lg.manager.zesty.io/schema/6-ba898da088-f04p2s
+  *
+  * Data Output Example: https://zesty.org/services/web-engine/introduction-to-parsley/parsley-index#tojson
+  * Images API: https://zesty.org/services/media-storage-micro-dam/on-the-fly-media-optimization-and-dynamic-image-manipulation
+  */
+ 
+ import React  from 'react';
+ 
+ function Article({ content }) {
+     return (
+         <>
+             {/* Zesty.io Output Example and accessible JSON object for this component. Delete or comment out when needed.  */}
+             <h1 dangerouslySetInnerHTML={{__html:content.meta.web.seo_meta_title}}></h1>
+             <div>{content.meta.web.seo_meta_description}</div>
+             {/* End of Zesty.io output example */}
+         </>
+     );
+ }
+ 
+ export default Article;
+ 
