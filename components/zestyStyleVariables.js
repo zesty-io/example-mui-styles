13e19a42b2357e722097f5bcbe601f3e5e18f3da
Bryan Estevez <55866499+b-estevez@users.noreply.github.com>
2023-05-05 02:45:07 +0800

    Update zestyStyleVariables.js

--- components/zestyStyleVariables.js
+++ components/zestyStyleVariables.js
@@ -5,9 +5,23 @@ const headerFontSize = styles['font-size-base'];
 export default function ZestyStyleVariables() {
   const zestySettingsTheme = createTheme({
     palette: {
+     primary: {
+        main: `${styles['brand-primary'] || '#42a5f5'}`,
+      },
       secondary: {
-        //Accessing the @brand-primary color
-        main: `${styles['brand-secondary'] || `#fff`}`,
+        main: `${styles['gray-base'] || '#ab47bc'}`,
+      },
+      success: {
+        main: `${styles['brand-success'] || '#388e3c'}`,
+      },
+      info: {
+        main: `${styles['brand-info'] || '#0288d1'}`,
+      },
+      warning: {
+        main: `${styles['brand-warning'] || '#f57c00'}`,
+      },
+      error: {
+        main: `${styles['brand-danger'] || '#d32f2f'}`,
       },
     },
 
